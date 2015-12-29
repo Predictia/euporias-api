4b7e6ccb2b844886c747590c51ecc5acec71c54e
Daniel San Martín Segura <daniel@predictia.es>
2015-12-29 14:40:57 +0100

    warnings suppressed

--- client/src/main/java/eu/euporias/RequestGenerator.java
+++ client/src/main/java/eu/euporias/RequestGenerator.java
@@ -3,7 +3,6 @@ package eu.euporias;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
